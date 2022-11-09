c6cc54af91ea1cff35a2247eafaeecb1b4ea6392
Juan Robi Lara <juanrobilara1@gmail.com>
2022-11-09 05:16:40 -0300

    agregue todo

---
+++ src/hola/holis.java
@@ -0,0 +1,29 @@
+package hola;
+
+public class holis {
+	
+
+	
+		private Integer edad = 5;
+
+		public holis(Integer edad) {
+			super();
+			this.edad = edad;
+		}
+
+		public Integer getEdad() {
+			return edad;
+		}
+
+		public void setEdad(Integer edad) {
+			this.edad = edad;
+		}
+		
+		public void decirHola() {
+			
+			System.out.println("hola");
+		}
+		
+	
+	
+}
